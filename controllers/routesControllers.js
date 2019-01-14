bfa0848ba525b7d1119298dbfdabaceffa4e59b3
Okaba Mac <markokaba99@gmail.com>
2019-01-14 10:52:31 +0100

    added verifcation forms

--- controllers/routesControllers.js
+++ controllers/routesControllers.js
@@ -1,5 +1,30 @@
-// export default class PagesControls {
-//     static getHomePage (req, res) {
-//         res.render();
-//     }
-// }
+const PagesControls = {
+    getHomePage: function (req, res) {
+        res.render('login');
+    },
+
+    adminLogin: function (req, res) {
+        res.render('admin-login');
+    },
+
+    registration: function (req, res) {
+        res.render('officers-panel')
+    },
+
+    login: function (req, res) {
+        const {email, password } = req.body;
+        console.log(req.body);
+        res.render('officers-panel');
+    },
+
+    adminPanel: function (req, res) {
+        res.render('admin-panel');
+    },
+
+    addUser: function (req, res) {
+        console.log(req.body);
+        res.send('User added successfully');
+    }
+
+};
+module.exports = PagesControls;
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,13 @@
-// import PagesControls from '../controllers/routesControllers';
+const PagesControls =  require('../controllers/routesControllers');
 const express = require('express');
 const router = express.Router();
 
 
-router.get('/', (req, res) => res.render('login'));
-router.get('/admin-login', (req, res) => res.render('admin-login'));
-router.get('/registration', (req, res) => res.render('officers-panel'));
-router.post('/login', (req, res) => {
-    const {email, password } = req.body;
-    console.log(req.body);
-    res.render('officers-panel');
-});
-router.post('/admin-panel', (req, res) => 
-res.render('admin-panel'));
+router.get('/', PagesControls.getHomePage);
+router.get('/admin-login', PagesControls.adminLogin);
+router.get('/registration', PagesControls.registration);
+router.post('/login', PagesControls.login);
+router.post('/admin-panel', PagesControls.adminPanel);
 
-router.post('/addUser', (req, res) => {
-    console.log(req.body);
-    res.send('User added successfully');
-});
+router.post('/addUser', PagesControls.addUser);
 module.exports = router;
