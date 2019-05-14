8d6d8e93cad1310057abac001fe9a72179b37d28
Okaba Mac <markokaba99@gmail.com>
2019-05-14 16:08:06 +0100

    Fix print seal

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -5,52 +5,50 @@ import Recertification from '../models/ReCertification';
 const Tests = [RoutineTest, TypeTest, Recertification];
 const options = ['RoutineTest', 'TypeTest', 'ReCertification'];
 const ReportControls = {
-  byState: async (req, res, next) => {
+  byState: (req, res) => {
     for (let i = 0; i < options.length; i++) {
       if (options[i] === req.body.test) {
-        try {
-          const data = Tests[i].find({
-            state: req.body.state,
-          });
-          return res.status(200).send({
+        Tests[i].find({
+          state: req.body.state,
+        }).then((data) => {
+          res.status(200).send({
             data,
           });
-        } catch (err) {
-          return res.status(400).send({
+        }).catch(() => {
+          res.status(400).send({
             status: 'Error',
             message: 'Please try again later',
           });
-        }
+        });
       }
     }
   },
 
-  byDate: async (req, res,) => {
+  byDate: (req, res) => {
     for (let i = 0; i < options.length; i++) {
       if (options[i] === req.body.test) {
-        try {
-          const data = Tests[i].find({
-            date: {
-              $gte: `${req.body.from}`,
-              $lt: `${req.body.to}`,
-            },
-          });
-          return res.status(200).send({
+        Tests[i].find({
+          date: {
+            $gte: `${req.body.from}`,
+            $lt: `${req.body.to}`,
+          },
+        }).then((data) => {
+          res.status(200).send({
             data,
           });
-        } catch (err) {
-          res.status(500).send({
+        }).catch(() => {
+          res.status(400).send({
             status: 'Error',
             message: 'Please try again later',
           });
-        }
+        });
       }
     }
   },
   byBatch: async (req, res) => {
     try {
       const data = await Tests[0].find({
-        batchId: req.body.batchId,
+        batchID: req.body.batchID,
       });
       return res.status(200).send({
         data,
